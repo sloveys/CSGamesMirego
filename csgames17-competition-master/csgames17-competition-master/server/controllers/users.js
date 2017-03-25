f2d523aa36222570cc12ee1b070a8e7a8cb214e0
sloveys <sloveys@mail.uoguelph.ca>
2017-03-25 16:44:52 -0400

    user not existing interception

--- csgames17-competition-master/csgames17-competition-master/server/controllers/users.js
+++ csgames17-competition-master/csgames17-competition-master/server/controllers/users.js
@@ -21,7 +21,20 @@ exports.getById = function (req, res) {
     if (user) {
       res.json(user);
     } else {
-      res.status(404).send('User not found');
+      if (req.body.username) { //  && req.body.password
+          let user = {
+            'username': req.body.username,
+            'password': "", // bcrypt.hashSync(req.body.password, 8)
+            'url': req.body.url || ''
+          };
+          req.db.users.insert(user, function (e, user) {
+            delete user.password;
+            res.json(user);
+          });
+        } else {
+          res.status(400).send('Missing required fields');
+        }
+      "res.status(404).send('User not found');"
     }
   });
 };
