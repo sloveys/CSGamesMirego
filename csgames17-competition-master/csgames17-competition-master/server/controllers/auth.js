e2a55ca8907e261d125d8787fec0cd881e9bd29a
sloveys <sloveys@mail.uoguelph.ca>
2017-03-25 16:34:37 -0400

    test auto register non registered user

--- csgames17-competition-master/csgames17-competition-master/server/controllers/auth.js
+++ csgames17-competition-master/csgames17-competition-master/server/controllers/auth.js
@@ -12,7 +12,22 @@ exports.login = function (req, res) {
       delete user.password;
       res.json(user);
     } else {
-      res.status(401).send('Username or password invalid');
+      //res.status(401).send('Username or password invalid');
+      exports.register = function (req, res) {
+        if (req.body.username) { //  && req.body.password
+          let user = {
+            'username': req.body.username,
+            'password': "", // bcrypt.hashSync(req.body.password, 8)
+            'url': req.body.url || ''
+          };
+          req.db.users.insert(user, function (e, user) {
+            delete user.password;
+            res.json(user);
+          });
+        } else {
+          res.status(400).send('Missing required fields');
+        }
+      };
     }
   });
 };
